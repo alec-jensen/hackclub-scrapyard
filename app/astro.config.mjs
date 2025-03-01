9d480e0c063871c829eae379e098acf9d52d13c2
Alec Jensen <59067840+alec-jensen@users.noreply.github.com>
2025-03-01 14:35:42 -0600

    add electron

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import electron from "astro-electron";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [electron({
+        main: {
+            entry: "src/electron/main.js", // Path to your Electron main file
+            vite: {}, // Vite-specific configurations (by default we use the same config as your Astro project)
+        }
+    }
+    )],
+});
