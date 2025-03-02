3df37e85d4de07ae1bc71715b1fc2b587f951b40
Alec Jensen <59067840+alec-jensen@users.noreply.github.com>
2025-03-01 23:35:36 -0600

    file explorer works

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -7,8 +8,19 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
